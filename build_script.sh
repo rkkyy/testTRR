cb5a9d6869547858ba0d7d7f77e555b6cf3afde4
Sonny Martin <34138711+rkkyy@users.noreply.github.com>
2017-12-03 19:02:37 +0000

    compile java in build script

--- build_script.sh
+++ build_script.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "build_script has run!"
+echo "------------------------- compiling java"
+javac HelloWorld.java
+echo "------------------------- running java"
+java HelloWorld
+echo "------------------------- build_script has finished!"
