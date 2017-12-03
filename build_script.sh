b827649fa1977f008610c82a9ec3c514c9f12dd1
Sonny Martin <34138711+rkkyy@users.noreply.github.com>
2017-12-03 19:15:05 +0000

    changed to python, with python file

--- build_script.sh
+++ build_script.sh
@@ -2,6 +2,8 @@
 
 echo "------------------------- compiling java"
 javac HelloWorld.java
-echo "------------------------- running java"
+echo "------------------------- running java" 
 java HelloWorld
+echo "------------------------- running python" 
+python HelloWorld.py
 echo "------------------------- build_script has finished!"
